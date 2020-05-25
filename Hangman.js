aefd7bd0a4ab8c27c7b15c75e2bfea65cc9ae101
Nijin <nijinazarkpz@gmail.com>
2020-05-25 15:35:28 -0700

    Main Functions Added

---
+++ Hangman.js
@@ -0,0 +1,57 @@
+class Hangman{
+    constructor(word,remainingGuess){   
+        this.word = word.toLowerCase().split('')
+        this.remainingGuess = remainingGuess
+        this.guessedLetter = []
+        this.status = 'Playing'
+    }
+
+    makeGuess(guess){
+        guess = guess.toLowerCase()
+        const uniqueGuess = !this.guessedLetter.includes(guess)
+        const badGuess = !this.word.includes(guess)
+
+        if(this.status !== 'Playing'){
+            return
+        }
+
+        if(uniqueGuess){
+            this.guessedLetter.push(guess)
+        }
+
+        if(uniqueGuess && badGuess){
+            this.remainingGuess--
+        }
+        this.recalculateStatus()
+    }   
+
+    getPuzzle(){
+
+        let puzzle = ''
+
+        this.word.forEach((letter) => {
+        if(this.guessedLetter.includes(letter)){
+            puzzle+=letter
+        }else{
+            puzzle+='*'
+        }
+        })
+
+        return puzzle
+    }
+
+    getStatusMessage(){
+        
+        if(this.status === 'Playing'){
+            return `Remaining Guess: ${this.remainingGuess}`
+        }else if(this.status === 'Finished'){
+            return `Success! You did it. - ${this.word.join('')}`
+        }else if(this.status === 'Failed'){
+            return `FAILED! Give it an another try.`
+        }
+    }
+
+    recalculateStatus(){
+
+    }
+}
