d90ef266dc1374f2a5b08d962a5c02dbd295d5a5
Nijin <nijinazarkpz@gmail.com>
2020-05-25 16:05:49 -0700

    Project at Intermediate Level

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const word = new Hangman('Dog',2)
+const puzzleText = document.querySelector('#puzzle')
+const remguess = document.querySelector('#guess')
+
+window.addEventListener('keypress', (e) => {
+    const guess = String.fromCharCode(e.charCode)
+    word.makeGuess(guess)
+
+    puzzleText.textContent = word.getPuzzle()
+    remguess.textContent = word.getStatusMessage()
+})
